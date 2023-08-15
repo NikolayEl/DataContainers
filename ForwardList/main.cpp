d399f76208eab842251c42f764045ef67a6b235a
Nikolay Elshin <nikolay19830223@gmail.com>
2023-08-15 19:02:23 +0800

    is done

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #define tab "\t"
+#define delimitr "\n---------------------------------------------------------------\n"
 class Element
 {
 	int Data; // значение элемента
@@ -33,9 +34,41 @@ public:
 		Head = nullptr; // Если список пуст, то его голова указывает на 0
 		cout << "LConstructor:\t" << this << endl;
 	}
+	ForwardList(const ForwardList& other)
+	{
+		Element* Temp = other.Head;
+		Head = new Element(Temp->Data, Head);
+		int count = 0;
+		while (Temp)
+		{
+			count++;
+			if (count == 1)
+			{
+				Temp = Temp->pNext;
+				continue;
+			}
+			push_back(Temp->Data);
+			Temp = Temp->pNext;
+		}
+		cout << "LCopyConstructor:\t" << this << endl;
+	}
+	ForwardList(ForwardList&& other)
+	{
+		this->Head = nullptr;
+		this->Head = other.Head;
+		cout << "LMoveConstructor:\t" << this << endl;
+	}
 	~ForwardList()
 	{
+		Element* Temp;
+		while (Head)
+		{
+			Temp = Head->pNext;
+			Head = nullptr;
+			Head = Temp;
+		}
 		cout << "LDestructor:\t" << this << endl;
+		print(); //Проверка на выполенение удаления
 	}
 
 	//					Adding elemetns;
@@ -50,8 +83,79 @@ public:
 			Temp = Temp->pNext;
 		Temp->pNext = new Element(Data);
 	}
+	void pop_front()
+	{
+		Head = Head->pNext;
+	}
+	void pop_back()
+	{
+		int count = 0;
+		Element* Temp = Head;
+		while (Temp->pNext)
+		{
+			Temp = Temp->pNext;
+			count++;
+		}
+		Temp = Head;
+		for (int i = 0; i < count - 1; i++) Temp = Temp->pNext;
+		Temp->pNext = nullptr;
+		
+	}
+
+	void insert(const int index, const int number)
+	{
+		int count = 0;
+		Element* Temp = Head;
+		if (index == 0)
+		{
+			Head = new Element(number, Head);
+		}
+		else 
+		{
+			while (count + 1 != index)
+			{
+				Temp = Temp->pNext;
+				count++;
+			}
+			Temp->pNext = new Element(number, Temp->pNext);
+		}
+	}
+
 
 	//					Methods:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		Element* Temp = other.Head;
+		Head = new Element(Temp->Data);
+		int count = 0;
+		while (Temp)
+		{
+			count++;
+			if (count == 1)
+			{
+				Temp = Temp->pNext;
+				continue;
+			}
+			push_back(Temp->Data);
+			Temp = Temp->pNext;
+		}
+		cout << "LCopyAssignment:\t" << this << endl;
+		return *this;
+	}
+	ForwardList& operator=(ForwardList&& other)
+	{
+		if (this == &other) return *this;
+		Element* Temp;
+		while (Head)
+		{
+			Temp = Head->pNext;
+			Head = nullptr;
+			Head = Temp;
+		}
+		Head = other.Head;
+		cout << "LMoveAssignment:\t" << this << endl;
+		return *this;
+	}
 	void print() const
 	{
 		Element* Temp = Head; //Temp - это итератор
@@ -76,6 +180,26 @@ void main()
 	{
 		list.push_front(rand() % 100);
 	}
+	cout << delimitr << endl;
 	list.push_back(123);
 	list.print();
+	cout << delimitr << endl;
+	list.push_front(123);
+	list.print();
+	cout << delimitr << endl;
+	list.pop_front();
+	list.print();
+	cout << delimitr << endl;
+	list.pop_back();
+	list.print();
+	cout << delimitr << endl;
+	list.insert(5, 321);
+	list.print();
+	cout << delimitr << endl;
+	ForwardList A(list);
+	A.print();
+	cout << delimitr << endl;
+	ForwardList B;
+	B = list;
+	B.print();
 }
