36e3774056a21a0cda94e7bcc3440518c868e419
Nikolay Elshin <nikolay19830223@gmail.com>
2023-08-20 22:31:16 +0800

    push_front, print, print_end

--- List2/main.cpp
+++ List2/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-
+#define delimitr "\n-----------------------------------------------------\n"
 class DoublyList;
 class Element
 {
@@ -37,11 +37,36 @@ public:
 		std::cout << "DDistructor:\t" << this << std::endl;
 	}
 	//								Method's
+	void push_front(int Data)
+	{
+		Element* Temp;
+		Temp = new Element(Data, nullptr, Head);
+		if (Head) Head->pPrev = Temp;
+		//if(Head)std::cout << "pPrev: \t" << Head->pPrev << std::endl; //сделал чтобы смотреть как записываются адреса
+		Head = Temp;
+		if (!this->Head->pNext) Tail = Head;
+		//std::cout << "Head: \t" << Head << std::endl;
+		//std::cout << "Tail: \t" << Tail << std::endl;
+		//std::cout << "pNext: \t" << Head->pNext << std::endl;
+	}
+	void push_back(int Data)
+	{
+
+	}
 	void print() const
 	{
 		std::cout << "Head:\t" << Head << std::endl;
 		std::cout << "Tail:\t" << Tail << std::endl;
-		for (Element* Temp = Head; Temp; Temp = Temp->pNext) std::cout << Temp->Data << " " << std::endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext) if(Temp)std::cout << Temp->Data << " " << std::endl;
+		std::cout << std::endl;
+		//std::cout << "Begin: \t" << Head << "\t" << Head->Data << std::endl;
+		//std::cout << "End: \t" << Tail << "\t" << Tail->Data << std::endl;
+	}
+	void print_end() const //Сделал для проверки Tail & pPrev
+	{
+		std::cout << "Head:\t" << Head << std::endl;
+		std::cout << "Tail:\t" << Tail << std::endl;
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev) if(Temp)std::cout << Temp->Data << " " << std::endl;
 		std::cout << std::endl;
 	}
 };
@@ -51,7 +76,16 @@ void main()
 {
 	setlocale(LC_ALL, "");
 #ifdef START_DOUBLY_LIST
-
+	int size;
+	std::cout << "Enter size you array: "; std::cin >> size;
+	DoublyList list;
+	for (int i = 0; i < size; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+	std::cout << delimitr << std::endl;
+	list.print_end();
 
 #endif // START_DOUBLY_LIST
 
